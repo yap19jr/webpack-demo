00738c2efeb769010ec47b9fa900b5648da316d0
Rodolfo Yap <yap19jr@gmail.com>
2022-12-05 01:31:25 +0800

    Install html-webpack-plugin and clean dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-  entry: './src/index.js',
+module.exports = {
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
-   output: {
-    filename: 'bundle.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+    path: path.resolve(__dirname, 'dist'),
+   clean: true,
+  },
+};
